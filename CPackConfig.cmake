de29915e35a357a9446819026d022b54198d0a8a
Roy Spliet <r.spliet@ultimaker.com>
2015-06-23 09:26:11 +0200

    Add CPack definition for protobuf3
    Make sure to build the shared modules too. Fixes the file name for libprotoc.
    
    Signed-off-by: Roy Spliet <r.spliet@ultimaker.com>

---
+++ CPackConfig.cmake
@@ -0,0 +1,24 @@
+set(CPACK_PACKAGE_VENDOR "Google")
+set(CPACK_PACKAGE_CONTACT "Arjen Hiemstra <a.hiemstra@ultimaker.com>")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Protobuf communication framework")
+set(CPACK_PACKAGE_VERSION_MAJOR 3)
+set(CPACK_PACKAGE_VERSION_MINOR 0)
+set(CPACK_PACKAGE_VERSION_PATCH 0)
+
+set(RPM_REQUIRES
+    "python3 (>= 3.4.0)"
+    "libgcc (>= 4.9.0)"
+    "libstdc++ (>= 4.9.0)"
+)
+string(REPLACE ";" "," RPM_REQUIRES "${RPM_REQUIRES}")
+set(CPACK_RPM_PACKAGE_REQUIRES ${RPM_REQUIRES})
+
+set(DEB_DEPENDS
+    "python3 (>= 3.4.0)"
+    "libgcc1 (>= 4.9.0)"
+    "libstdc++6 (>= 4.9.0)"
+)
+string(REPLACE ";" ", " DEB_DEPENDS "${DEB_DEPENDS}")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS ${DEB_DEPENDS})
+
+include(CPack)
